88de69342d2cad0efbff5fcd90e359ecfd4be936
Htut Myat Ag <htutmyataung549@gmail.com>
2026-06-08 10:56:55 +0630

    add: repo

--- src/main/java/com/example/event_booking_system/EventBookingSystemApplication.java
+++ src/main/java/com/example/event_booking_system/EventBookingSystemApplication.java
@@ -2,8 +2,10 @@ package com.example.event_booking_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.event_booking_system")
 public class EventBookingSystemApplication {
 
 	public static void main(String[] args) {
